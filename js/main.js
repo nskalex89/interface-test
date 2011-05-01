b8650d6e9deee75f0f3668933c6ac630e7fcb25c
unknown <Alexey@.(none)>
2011-05-01 20:58:35 +0700

    first commit

---
+++ js/main.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+    d1 = [
+        [0, 1],
+        [1, 2],
+        [2, 3],
+        [3, 4],
+        [4, 5],
+        [5, 4],
+        [6, 3],
+        [7, 2],
+        [8, 1]
+    ];
+
+    $.plot($("#placeholder"), [
+        { data: d1, color: "green" }
+    ],
+    {
+        bars: { show: true, barWidth: 0.9, fill: 0.7 },
+        xaxis: { ticks: [], autoscaleMargin: 0.02 }
+    });
+});
